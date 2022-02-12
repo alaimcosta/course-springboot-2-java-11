a25f1cfdad360434cb74da56b7fee12cf18e25c9
alaimcosta <alaim.costa23@gmail.com>
2022-02-11 23:17:37 -0300

    Exception handling - update

--- src/main/java/br/com/altec/course/services/UserService.java
+++ src/main/java/br/com/altec/course/services/UserService.java
@@ -3,6 +3,8 @@ package br.com.altec.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -50,9 +52,14 @@ public class UserService {
 	//Atualizar 
 	//Recebo um usuário
 	public User update(Long id, User obj) {
-		User entity = repository.getOne(id);//não vai no BD, só para ponitorar o objeto
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getOne(id);//não vai no BD, só para ponitorar o objeto
+			updateData(entity, obj);
+			return repository.save(entity);	
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		
 	}
 
 	private void updateData(User entity, User obj) {
